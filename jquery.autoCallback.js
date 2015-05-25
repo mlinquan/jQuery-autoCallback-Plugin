928b098405fefdac0bb92006d53ac08d56ba0669
LinQuan <linquan@hexindai.com>
2015-05-25 16:13:27 +0800

    add demo page and fixed some bug

--- jquery.autoCallback.js
+++ jquery.autoCallback.js
@@ -16,46 +16,36 @@
     $.each(['append', 'prepend', 'before', 'after', 'replaceWith', 'appendTo', 'prependTo', 'insertBefore', 'insertAfter', 'replaceAll'], function(i, name) {
         $.domEvents[name] = $.fn[name];
         $.fn[name] = function() {
-            $.domEvents[name].apply(this, arguments);
+            var back_elem;
             this.domManip(arguments, function( elem ) {
                 var that = this;
-                if(i > 4) {
-                    $(elem).autoCallback(that);
-                } else {
-                    $(that).autoCallback(elem);
+                if(!elem.jquery) {
+                    elem = $(elem);
                 }
+                if(i > 4) {
+                    $(elem).autoCallback(this);
+                } else {
+                    $(this).autoCallback(elem);
+                }
+                back_elem = elem;
             });
-            return this;
+            return $.domEvents[name].apply(this, back_elem);
         };
     });
     $.autoCallback = {
     };
     $.fn.autoCallback = function(elem) {
-        var that = this;
+        var that = $(this);
+        elem = $(elem);
         $.each($.autoCallback, function() {
             this.require(that, elem) && this.callback(that, elem);
         });
     };
     $(function() {
         $.each($.autoCallback, function(i, name) {
-            var that = this,
+            var that = $(this),
             obj = $(document);
             that.domready && that.callback(obj, that);
         });
     })
-})(jQuery);
-
-(function($) {
-    $.autoCallback.demo = {
-        domready: true,
-        require: function(obj, elem) {
-            return ($(obj).is("body") && $(elem).is("a"));
-        },
-        callback: function(obj, elem) {
-            $(elem).on("click", function() {
-                console.log($(this).text());
-                return false;
-            });
-        }
-    };
 })(jQuery);
